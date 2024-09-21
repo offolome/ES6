b0d48ab19b74040566d37dedc30deba8d2b840e7
unknown <offado>
2024-09-21 09:38:09 +0000

    Exercices

--- exo1.js
+++ exo1.js
@@ -6,3 +6,8 @@
 function evenOrOdd(number) {
     return number % 2 === 0 ? 'Even' : 'Odd';
 }
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(7));
+console.log(evenOrOdd(5));
+console.log(evenOrOdd(6));
